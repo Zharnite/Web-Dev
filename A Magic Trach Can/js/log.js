4dd479de2f0bd5b1184984c54c20686caa9e2182
Zhen Yi Pan <30640893+Zharnite@users.noreply.github.com>
2018-01-29 11:53:47 -0500

    updated log

---
+++ A Magic Trach Can/js/log.js
@@ -0,0 +1,6 @@
+logEntries = document.getElementById("logEntries");
+li_1 = document.createElement("li");
+entry_1 = "That Thing: There is a trash can in front of me.";
+text_1 = document.createTextNode(entry_1);
+li_1.appendChild(text_1);
+logEntries.appendChild(li_1);
